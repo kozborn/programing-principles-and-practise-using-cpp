d9d35731362faae965bb3810fc797f7c5eff6b90
Piotr Kozubek <piotrkozubek@gmail.com>
2018-03-25 19:17:48 +0200

    Chapter 9

--- Chapter9/Exercise-9-2/main.cpp
+++ Chapter9/Exercise-9-2/main.cpp
@@ -5,26 +5,44 @@ class NamePairs {
     void read_names();
     void read_ages();
     void print();
+    void sort();
   private:
     vector<string> names;
     vector<int> ages;
 };
 
 void NamePairs::read_names() {
+  cout << "Please provide names and end input with ';'" << endl;
   string name;
   while (true)
   {
     cin >> name;
-    if(name == "EOF") break;
+    if (name == ";") break;
     names.push_back(name);
   }
 }
 
 void NamePairs::read_ages() {
+  cout << "For each name please provide age" << endl;
   int age;
   for (int i = 0; i < names.size(); ++i) {
+    cout << names[i] << ": " ;
     cin >> age;
     ages.push_back(age);
+  }
+}
+
+void NamePairs::sort() {
+  if(names.size() != ages.size()) {
+    cout << "Size of names vector and ages vector does not match" << endl;
+  }
+  vector<string> namesUnSorted = names;
+  vector<int> agesUnSorted = ages;
+  std::sort(names.begin(), names.end());
+  vector<string>::const_iterator pos;
+  for(int i = 0; i < names.size(); ++i) {
+    pos = std::find(namesUnSorted.begin(), namesUnSorted.end(), names.at(i));
+    ages.at(i) = agesUnSorted.at(pos);
   }
 }
 
