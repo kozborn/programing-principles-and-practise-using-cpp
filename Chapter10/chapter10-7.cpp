2f309b153969fb81e595cb51369b879f9e59172c
kozborn <piotrkozubek@gmail.com>
2018-03-27 22:49:43 +0200

    Reading int from istream

--- Chapter10/chapter10-7.cpp
+++ Chapter10/chapter10-7.cpp
@@ -1,15 +1,33 @@
 #include "../std_lib_facilities.h"
 
+void skip_to_int() {
+  if (cin.fail())
+  {
+    cin.clear();
+    cout << "To nie była liczba" << endl;
+    char ch;
+    while (cin >> ch && !isdigit(ch));
+    if (!cin) error("Brak danych");
+    cin.unget();
+  }
+}
+
 int main() {
   try {
     int i = 0;
     cout << "Provide a number between 1 and 10" << endl;
 
-    while(cin >> i) {
-      if (1 <=i && i <= 10) {
-        cout << "Well done, I'm proud of you" << endl;
+    while(true) {
+      if (cin >> i)
+      {
+        if (1 > i || i > 10)
+        {
+          cout << "Sorry but " << i << " doesn't belong to [1,10]" << endl;
+        } else {
+          cout << "Ok" << endl;
+        }
       } else {
-        cout << "Unfortunately " << i << " doesn't match range 1 to 10 " << endl;
+        skip_to_int(); 
       }
     }
   } catch(...) {
