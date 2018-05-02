a5d6c24b324a422c021ba9776e4a527628b4cbcf
Piotr Kozubek <piotrkozubek@gmail.com>
2018-05-02 22:59:21 +0200

    Improved makefile

--- Chapter12/Exercise12-3/main.cpp
+++ Chapter12/Exercise12-3/main.cpp
@@ -8,16 +8,13 @@ int main() {
 
     Point tl;
 
-    Simple_window win(tl, 600, 400, "Canvas");
+    Simple_window win(tl, 600, 400, "Dupa zbita");
 
     Polygon poly;
 
-    poly.add(Point{300, 200});
+    poly.add(Point{200, 200});
     poly.add(Point{350, 100});
-    poly.add(Point{400, 100});
-    poly.add(Point{400, 200});
-    poly.add(Point{300, 230});
-    poly.add(Point{500, 330});
+    poly.add(Point{350, 200});
 
     poly.set_color(Color::red);
 
